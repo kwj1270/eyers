d61a81575bba40ce96c3812e7f3e15b02b3e942e
gim-ujae <kwj1270@naver.com>
2020-03-16 19:24:17 +0900

    Initial commit

---
+++ src/main/java/net/iwinv/eyers1/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package net.iwinv.eyers1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
+
---
+++ src/main/java/net/iwinv/eyers1/domain/user/User.java
@@ -0,0 +1,51 @@
+package net.iwinv.eyers1.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_seq;
+
+    @Column(nullable = false, unique = true)
+    private String user_id;
+
+    @Column(nullable = false)
+    private String user_pw;
+
+    @Column(nullable = false)
+    private String user_name;
+
+    @Column(nullable = false, unique = true)
+    private String user_studentnumber;
+
+    @Column(nullable = false, unique = true)
+    private String user_nickname;
+
+    @Builder
+    public User(String user_id, String user_pw, String user_name, String user_studentnumber, String user_nickname){
+        this.user_id = user_id;
+        this.user_pw = user_pw;
+        this.user_name = user_name;
+        this.user_studentnumber = user_studentnumber;
+        this.user_nickname = user_nickname;
+    }
+
+    public User update(String user_id, String user_pw, String user_name, String user_nickname){
+        this.user_id = user_id;
+        this.user_pw = user_pw;
+        this.user_name = user_name;
+        this.user_nickname = user_nickname;
+
+        return this;
+    }
+
+}
